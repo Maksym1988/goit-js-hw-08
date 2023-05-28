bc693a9d5347a45f1e5c484d1b19886ea9b8df8d
Maksym1988 <splien1988@gmail.com>
2023-05-29 00:03:11 +0100

    update task-2 and task-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const CURRENTTIME_KEY = 'videoplayer-current-time';
+
 player.on(
   'timeupdate',
   throttle(data => {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+    localStorage.setItem(CURRENTTIME_KEY, data.seconds);
   }, 1000)
 );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem(CURRENTTIME_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const FEEDBACK_KEY = 'feedback-form-state';
+
+feedbackForm.addEventListener('input', throttle(saveDataValue, 500));
+const { email, message } = feedbackForm.elements;
+
+function saveDataValue() {
+  const dataValue = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(dataValue));
+}
+
+window.addEventListener('load', loadDataValue);
+function loadDataValue() {
+  const getDataValue = JSON.parse(localStorage.getItem(FEEDBACK_KEY)) || {};
+
+  email.value = getDataValue.email || '';
+  message.value = getDataValue.message || '';
+}
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+function onFormSubmit(eve) {
+  eve.preventDefault();
+
+  const userFeedback = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+  console.log(userFeedback);
+
+  localStorage.removeItem(FEEDBACK_KEY);
+  feedbackForm.reset();
+}
